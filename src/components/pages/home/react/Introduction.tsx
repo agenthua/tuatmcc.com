725c10541a928bfd68b685e1aa183850d64576dc
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-08-21 23:09:37 +0900

    feat: add footer(仮), fix inView animation

--- src/components/pages/home/react/Introduction.tsx
+++
@@ -1,9 +0,0 @@
-import { SlideIn } from "./SlideIn";
-
-export const Introduction = () => (
-  <div className="relative flex w-full flex-col">
-    <div className="mx-auto flex w-full max-w-[1200px] p-4">
-      <SlideIn />
-    </div>
-  </div>
-);
